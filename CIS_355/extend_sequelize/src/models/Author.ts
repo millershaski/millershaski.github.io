4e71f0b06eda33542396460345df7857e781f598
Tyler Millershaski <millershaski@gmail.com>
2025-03-19 13:18:06 -0400

    Added attributes and relationship to Author

--- CIS_355/extend_sequelize/src/models/Author.ts
+++ CIS_355/extend_sequelize/src/models/Author.ts
@@ -3,14 +3,46 @@ import sequelize from '../config/database';
 import Book from './Book';
 
 
-// TODO: Implement the Author model
-class Author extends Model {
+class Author extends Model 
+{
+    declare name: string;
+    declare bio: string;
+    declare birthYear: number;
 }
 
-// TODO: Initialize the Author model
 Author.init(
+{
+    id:
+    {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name:
+    {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    bio:
+    {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    birthYear:
+    {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+},
+{
+    sequelize, 
+    modelName: "Author",
+    tableName: 'Authors',  // this can be omitted, as the name will automatically be made the plural version of the modelName
+    timestamps: false, // I don't care when the records were created or updated 
+    underscored: true
+}
 );
 
-// TODO: Define associations
+Author.hasMany(Book);
 
 export default Author; 
